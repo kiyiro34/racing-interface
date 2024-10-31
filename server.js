0bf7db082d5bbbc52d2ede33200b545bccea33c8
kiyiro34 <119161574+kiyiro34@users.noreply.github.com>
2024-10-31 19:53:44 +0800

    fix: stylesheet

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
-// server.js
 const express = require('express');
 const path = require('path');
 const app = express();
 
-// Définir le port
 const PORT = process.env.PORT || 3000;
 
-// Servir les fichiers statiques du dossier "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route de base
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Démarrer le serveur
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
